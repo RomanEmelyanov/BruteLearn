802f64f7c2cc803f3f776e0fa4c62dcf01761bd8
RomanEmelyanov <RomanEmelyanov@users.noreply.github.com>
2021-08-16 11:32:09 +0300

    key size added

--- hashgen.java
+++ hashgen.java
@@ -5,11 +5,11 @@ This is small tool to debug hashes for keycloak
 COMPILE: javac hashgen.java
 RUN: java -cp . hashgen {Password} {SaltInBase64} {Iterations} {PBKDF2_ALGORITHM}
 Examples:
-1) java -cp . hashgen hashcat bYjGueAuZgUG2URhIui3zg== 20000 PBKDF2WithHmacSHA1
+1) java -cp . hashgen hashcat bYjGueAuZgUG2URhIui3zg== 20000 512 PBKDF2WithHmacSHA1
 Hash: Ir7uS0o9ouav1VCly0801VyioaQ/1sjwViHFmgD4kakDzzI39fRSifJutjbkd33U914v9QMVy+pF5k+xp+86cQ==
-2) java -cp . hashgen hashcat bYjGueAuZgUG2URhIui3zg== 100000 PBKDF2WithHmacSHA256
+2) java -cp . hashgen hashcat bYjGueAuZgUG2URhIui3zg== 100000 512 PBKDF2WithHmacSHA256
 Hash: 1IqX6hPxxAGwf/aZUUTKpZzf7MLYTLWNFTtGwTfwQA9mU04tA8M4Y1d2yAwF6MFWOiYZpEnIX//RdsY81UGnQw==
-3) java -cp . hashgen hashcat bYjGueAuZgUG2URhIui3zg== 27500 PBKDF2WithHmacSHA512
+3) java -cp . hashgen hashcat bYjGueAuZgUG2URhIui3zg== 27500 512 PBKDF2WithHmacSHA512
 Hash: MTkajtRaak3UDWUlYANtTt/D+CpYSq9OK0Yzs5WTz32eydupgcNE88f8SMNUhCHtwGiFVkfTKAIZynCa3i46Iw==
 */
 
@@ -20,14 +20,14 @@ import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
 public class hashgen {
- public static final int DERIVED_KEY_SIZE = 512;
  public static void main(String[] args) throws Exception {
   try{
-   String p = args[0]; System.out.println("Password: " + p);
-   String s = args[1]; System.out.println("Salt: " + s);
-   int i = Integer.parseInt(args[2]); System.out.println("Iterations: " + i);
-   String a = args[3]; System.out.println("PBKDF2_Algorithm: " + a);
-   PBEKeySpec spec = new PBEKeySpec(p.toCharArray(), Base64.getDecoder().decode(s), i, DERIVED_KEY_SIZE);
+   String p = args[0]; System.out.print("Password: " + p);
+   String s = args[1]; System.out.print(" Salt: " + s);
+   int i = Integer.parseInt(args[2]); System.out.print(" Iterations: " + i);
+   int k = Integer.parseInt(args[3]); System.out.print(" Key size: " + k);
+   String a = args[4]; System.out.println(" PBKDF2_Algorithm: " + a);
+   PBEKeySpec spec = new PBEKeySpec(p.toCharArray(), Base64.getDecoder().decode(s), i, k);
    SecretKeyFactory skf = null;
    try { skf = SecretKeyFactory.getInstance(a); } 
    catch (NoSuchAlgorithmException ignore) { System.out.println("NoSuchAlgorithmException: " + a); }
