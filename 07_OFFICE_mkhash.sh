257ab5650134412e9982a131a49619286cc32f02
RomanEmelyanov <RomanEmelyanov@users.noreply.github.com>
2023-09-14 16:21:30 +0300

    Add files via upload

--- 07_OFFICE_mkhash.sh
+++ 07_OFFICE_mkhash.sh
@@ -1,4 +1,15 @@
 echo Algo: MS Office 2013 
 echo Pass: $1
-./JtR/office2john.py test.docx | cut -d ':' -f 2 > office_hash.txt
+
+# apt install pandoc
+# echo "Test" | pandoc -o test.docx
+# pip install msoffcrypto-tool
+echo 'Set password manually'
+
+if [ ! -f "./office2john.py" ]; then
+ wget https://raw.githubusercontent.com/openwall/john/bleeding-jumbo/run/office2john.py
+ chmod +x ./office2john.py
+fi
+
+./office2john.py test.docx | cut -d ':' -f 2 > office_hash.txt
 cat office_hash.txt
