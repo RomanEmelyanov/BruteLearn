b6d36c046e3f57c59ae67ceb91adebcc724d22d8
RomanEmelyanov <RomanEmelyanov@users.noreply.github.com>
2023-09-14 15:28:22 +0300

    Add files via upload

--- 09_PDF_mkhash.sh
+++ 09_PDF_mkhash.sh
@@ -1,3 +1,6 @@
+
+# apt install imagemagic qpdf
+
 if [ ! -x "/usr/bin/convert" ]; then
  echo "ERROR: ImageMagick (convert) required"
  exit
@@ -5,19 +8,15 @@ fi
 
 echo Algo: PDF 
 echo Pass: $1
-rm -rf test.ps test.pdf
+rm -rf test.pdf
 
-if [ 0 ]; then
- convert xc:none -page Letter test.ps
- # ps2pdf12 -sOwnerPassword=$1 test.ps test.pdf
- ps2pdf13 -sOwnerPassword=$1 -sUserPassword=$1 test.ps test.pdf
- # ps2pdf14 -dEncryptionR=3 -dKeylength=128 -sOwnerPassword=$1 -sUserPassword=$1 test.ps test.pdf
- ./JtR/pdf2john.pl test.pdf | cut -f 2 -d ':' | sed s/\*4294967292/*-4/ > pdf_hash.txt
-else
+# on error: 'convert-im6.q16: attempt to perform an operation not allowed by the security policy `PDF' @ error/constitute.c/IsCoderAuthorized/421.'
+# <policy domain="coder" rights="read | write" pattern="PDF" />
+# just before </policymap> in /etc/ImageMagick-7/policy.xml 
+
  convert xc:none -page A4 test.pdf
- qpdf --encrypt $1 $1 40 -- test.pdf test1.pdf
- # qpdf --encrypt $1 $1 256 --use-aes=y --force-version=1.7 -- test.pdf test1.pdf
+ qpdf --encrypt $1 $1 256 -- test.pdf test1.pdf
  mv test1.pdf test.pdf
  ./JtR/pdf2john.pl test.pdf | cut -f 2 -d ':' > pdf_hash.txt
-fi
+
 cat pdf_hash.txt
--- 10_PDF_brute.sh
+++ 10_PDF_brute.sh
@@ -1,5 +1,6 @@
-rm -rf /Users/ers/.hashcat/hashcat.potfile
-hashcat --force -m 10400 -a 3 pdf_hash.txt ?d?d?d?d
+rm -rf $HOME/.hashcat/hashcat.potfile
+# https://hashcat.net/wiki/doku.php?id=example_hashes
+hashcat --potfile-disable -m 10700 -a 3 pdf_hash.txt -i ?d?d?d?d?d
 
 #rm -rf ./JtR/john.pot
 #./JtR/john --incremental=digits pdf_hash.txt
